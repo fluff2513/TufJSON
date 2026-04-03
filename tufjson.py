dd8970408c11b5ca7a49347037fa24c2772389a8
Fluff <fluffitofluffyfluff@gmail.com>
2026-04-03 13:42:24 -0400

    Upload tufjson.py

---
+++ tufjson.py
@@ -0,0 +1,17 @@
+import json
+import questionary
+from rich import print
+import subprocess
+
+subprocess.run("clear")
+print("[cyan1]T[/cyan1][cyan2]u[/cyan2][medium_spring_green]f[/medium_spring_green][spring_green1]G[/spring_green1][spring_green2]I[/spring_green2][green1]T[/green1] - The [bold]TUFFIEST[/bold] .gitignore file formater!")
+json_ = questionary.text("Insert the JSON text :")
+name = questionary.text("What is the file name? :")
+dictionnary = json.loads(json_)
+formate = json.dumps(dictionnary, indent=4)
+
+with open(f"{name}.json", "w") as f:
+    f.write(formate)
+    
+print(f"""[bright_green]Your JSON file succesfully create! :)[/bright_green]
+Made by @Fluff2513 on GitHub.""")
