507b2be8aff88b0c616343ff80d200469a65674e
Fluff <fluffitofluffyfluff@gmail.com>
2026-04-03 13:50:44 -0400

    Update tufjson.py

--- tufjson.py
+++ tufjson.py
@@ -4,7 +4,7 @@ from rich import print
 import subprocess
 
 subprocess.run("clear")
-print("[cyan1]T[/cyan1][cyan2]u[/cyan2][medium_spring_green]f[/medium_spring_green][spring_green1]G[/spring_green1][spring_green2]I[/spring_green2][green1]T[/green1] - The [bold]TUFFIEST[/bold] JSON file formater!")
+print("[cyan1]T[/cyan1][cyan2]u[/cyan2][medium_spring_green]f[/medium_spring_green][spring_green1]J[/spring_green1][spring_green2]S[/spring_green2][green1]O[/green1][green3]N[/green3] - The [bold]TUFFIEST[/bold] JSON file formater!")
 json_ = questionary.text("Insert the JSON text :").ask()
 name = questionary.text("What is the file name? :").ask()
 dictionnary = json.loads(json_)
